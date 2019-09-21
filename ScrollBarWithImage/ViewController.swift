9678002f4d4a8bff81748c0a14a26007db3b50c0
Takuya Aso <milanista224@gmail.com>
2019-09-21 20:40:07 +0900

    Imp TableView

--- ScrollBarWithImage/ViewController.swift
+++ ScrollBarWithImage/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let cellName = "Cell"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "ScrollBar with Image"
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+}
 
+extension ViewController: UITableViewDelegate {
 
 }
 
+extension ViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath)
+        cell.textLabel?.text = "Cell " + String(indexPath.row)
+        return cell
+    }
+}
